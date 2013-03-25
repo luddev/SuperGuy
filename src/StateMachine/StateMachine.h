aafe34c22d75cc5417701ec757b52b447208d8ee
ludkiller <ludkiller@dev.(none)>
2013-03-25 16:51:31 +0530

    Added State Machine and More Definitions

--- src/StateMachine/StateMachine.h
+++ src/StateMachine/StateMachine.h
@@ -13,80 +13,92 @@
  *  and the triggering condition for each transition.
  */
 
-
 #ifndef STATEMACHINE_H_
 #define STATEMACHINE_H_
+#include<stdio.h>
+/// States for State Machine
 enum STATES {
-	STATE_NULL,	//NULL STATE
-	STATE_INTRO, //INTRO or SPLASH SCREEN STATE
-	STATE_TITLE, //TITLE STATE
-	STATE_STAGE1, //STAGE1 can add other Stages later similar to STAGE1
-	STATE_GAMEOVER, //GAMEOVER self explanatory
-	STATE_QUIT //QUIT After Game over or random quit ;3
-}; // States for State Machine
+	STATE_NULL,	///NULL STATE
+	STATE_INTRO, ///INTRO or SPLASH SCREEN STATE
+	STATE_TITLE, ///TITLE STATE
+	STATE_OVERWORLD,
+	STATE_STAGE1, ///STAGE1 can add other Stages later similar to STAGE1
+	STATE_STAGE2,
+	STATE_GAMEOVER, ///GAMEOVER self explanatory
+	STATE_QUIT ///QUIT After Game over or random quit ;3
+};
+
 
 class stateMachine {
 public:
-	virtual void handle_events(); //Handle events for every state
-	virtual void logic(); //Handle Logic for each state
-	virtual void render(); //Handle rendering for each state
-	virtual ~stateMachine(); //Destructor for each state
-	void setNextState();	//Sets our next state
-	void changeState();		//Changes State
-private:
-	int stateID;	//Current state ID any value of enum STATES
-	int nextState;	//Next state ID
-	stateMachine *currentState; //pointer to current state
+	virtual void handle_events(); ///Handle events for every state
+	virtual void logic(); ///Handle Logic for each state
+	virtual void render(); ///Handle rendering for each state
+	virtual ~stateMachine(); ///Destructor for each state
 };
 
 class Intro: public stateMachine {
 private:
-	SDL_Surface *background;
-	SDL_Surface *message;
+	SDL_Surface *background; ///Intro Background
+	SDL_Surface *message; ///Intro Message
 public:
-	Intro();
-	~Intro();
-	void handle_events();
-	void logic();
-	void render();
+	Intro(); ///Constructor for Intro
+	~Intro(); ///Destructor for Intro
+	void handle_events(); ///Handle Intro Events
+	void logic(); ///Handle Intro Logic
+	void render(); ///Handle Intro Rendering
 
 };
 
 class Title: public stateMachine {
 private:
-	SDL_Surface *background;
-	SDL_Surface *message;
+	SDL_Surface *background; ///Title Background
+	SDL_Surface *message; ///Title Message
 public:
-	Title();
-	~Title();
-	void handle_events();
-	void logic();
-	void render();
+	Title(); ///Constructor for Title
+	~Title(); ///Destructor for Title
+	void handle_events(); ///Handle Title Events
+	void logic(); ///Handle Title Logic
+	void render(); ///Handle Title Rendering
 
+};
+
+class OverWorld:public stateMachine {
+	OverWorld(int);
+	~OverWorld();
 };
 class Stage1: public stateMachine {
 private:
-	SDL_Surface *background;
-	const static int levelWidth = 1024;
-	const static int levelHeight = 768;
+	SDL_Surface *background; ///Background of Stage1
+	const static int levelWidth = 1024; ///Width Of Stage1
+	const static int levelHeight = 768; ///Height Of Stage1
 public:
-	Stage1();
-	~Stage1();
-	void handle_events();
-	void logic();
-	void render();
+	Stage1(); ///Constructor for Stage1
+	~Stage1(); ///Destructor for Stage1
+	void handle_events(); ///Handle Stage1 Events
+	void logic(); ///Handle Stage1 Logic
+	void render(); ///Handle Stage1 Rendering
 
 };
 class GameOver: public stateMachine {
 private:
-	SDL_Surface *background;
-	SDL_Surface *message;
+	SDL_Surface *background; ///Background for GameOver
+	SDL_Surface *message; ///Background for Message
 public:
-	GameOver();
-	~GameOver();
-	void handle_events();
-	void logic();
-	void render();
+	GameOver(); ///Constructor for GameOver
+	~GameOver(); ///Destructor for GameOver
+	void handle_events(); ///Handle GameOver Events
+	void logic(); ///Handle GameOver Logic
+	void render(); ///Handle GameOver Rendering
 
 };
+
+
+int stateID = STATE_NULL;
+int nextState = STATE_NULL;
+stateMachine *currentState = NULL;
+
+void setNextState(int newState);	///Sets our next state
+void changeState();		///Changes State
+
 #endif /* STATEMACHINE_H_ */
